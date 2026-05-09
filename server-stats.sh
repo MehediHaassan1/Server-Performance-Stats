74a1f015fd7c199d79be0266396d6a672cf05906
mehedihassanm360ict <mehedi.haassan1@gmail.com>
2026-05-09 12:33:46 +0600

    total cpu usage

--- server-stats.sh
+++ server-stats.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+echo "========================================"
+echo "        SERVER PERFORMANCE STATS        "
+echo "========================================"
+
 echo "💻 Hostname : $(hostname)"
 echo "📆 Date: $(date)"
 echo "🕑 Uptime : $(uptime -p)"
 
 
+echo ""
+echo "==============="
+echo "Total CPU Usage"
+echo "==============="
+
+IDLE_CPU=$(top -bn1 | grep '%Cpu(s)' | awk -F',' '{print $4}' | awk -F' ' '{print $1}' | cut -d. -f1)
+
+TOTAL_CPU_USE=$((100-IDLE_CPU))
+
+echo "Total Cpu Use = ${TOTAL_CPU_USE}%"
+
+echo ""
+echo "=================="
+echo "Total memory usage"
+echo "=================="
